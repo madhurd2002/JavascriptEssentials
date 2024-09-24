c15c207c99cf0f83adc366aa5a40a65e07150ef0
madhurd2002 <madhur.dukhande9@gmail.com>
2024-09-24 03:50:56 -0400

    done the changes

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,29 @@
+let userRole = "manager";
+let accessLevel;
+
+if (userRole === " admin"){
+    accessLevel = "full acess gurantee";
+} else if (userRole === "manager"){
+    accessLevel = "limited access granted";
+} else {
+    accessLevel = "no access";
+}
+
+console.log("access level", accessLevel);
+
+
+let isLoggedIn= false;
+let userMessage
+
+if (isLoggedIn){
+    if(userRole === "no admin"){
+        userMessage = "welcome admin";
+    } else{
+        userMessage = "welcome, user";
+    }
+
+    } else{
+        userMessage = "please log in to access";
+    }
+
+    console.log("user message:", userMessage)
